de7b04f403d7a026c02af61d8c89cadf886dee2d
binh tran <binh.tran@8bitrockr.com>
2018-08-17 16:48:06 +0700

    implement scss

---
+++ src/App.js
@@ -0,0 +1,6 @@
+/**
+ * Created by binh on 8/17/18.
+ */
+export default {
+  name: 'App',
+};
--- src/App.vue
+++ src/App.vue
@@ -4,20 +4,7 @@
     <router-view/>
   </div>
 </template>
-
-<script>
-export default {
-  name: 'App',
-};
-</script>
-
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<script src="./App.js"></script>
+<style lang="scss" scoped>
+  @import './App.scss';
 </style>
