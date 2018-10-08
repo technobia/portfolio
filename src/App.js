d7cf77e75f47cc97428f043db30be7263b1f9aaa
binh tran <binh.tran@8bitrockr.com>
2018-10-08 16:49:35 +0700

    use portfolio theme

--- src/App.js
+++
@@ -1,6 +0,0 @@
-/**
- * Created by binh on 8/17/18.
- */
-export default {
-  name: 'App',
-};
--- src/App.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <div id="app">
-    <router-view/>
-  </div>
-</template>
-<script src="./App.js"></script>
-<style lang="scss" scoped>
-  @import './App.scss';
-</style>
